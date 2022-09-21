46592d867790bdd7875bd2746d11da94693e1bcb
cbdeboef <cbdeboef@co1313-03.ece.iastate.edu>
2022-09-21 13:06:27 -0500

    All broken :(

--- CFuncs/chebyshev2.c
+++ CFuncs/chebyshev2.c
@@ -49,26 +49,31 @@ void calcPoly(int N, double b[], double pts[], int numPts, double* y){
 	for(i = 0; i < numPts; i++){
 		float x = pts[i];	
 		for(k = 0; k < N; k++){
-			if(k == 0){ phi = 1;}
-			else if(k == 1){ phi = x;}
-			else if(k == 2){ phi = 2 * pow(x,2) - 1;}
-			else if(k == 3){ phi = 4 * pow(x,3) - 3 * x;}
-			else if(k == 4){ phi = 8 * pow(x,4) - 8 * pow(x,2) + 1;}
-			else if(k == 5){ phi = 16 * pow(x,5) - 20 * pow(x,3) + 5 * x;}
+			switch(k){
+				case 0:
+					phi = 1;
+				case 1:
+					phi = x;
+				case 2:
+					phi = 2 * pow(x,2) - 1;
+				case 3:
+					phi = 4 * pow(x,3) - 3 * x;
+				case 4:
+					phi = 8 * pow(x,4) - 8 * pow(x,2) + 1;
+				case 5:
+					phi = 16 * pow(x,5) - 20 * pow(x,3) + 5 * x;
 			
-			y[i] += b[k] * phi;	
+				y[i] += b[k] * phi;
+			}	
 		}
 	}
 }
 void writePolly(int numPts, double pts[], double y[]){
 	FILE *fptr;
-printf("\n1\n");
 	int i;
-	fptr = fopen("U:/CprE424/chebychev1.txt","w+");
-printf("\n2\n");
+	fptr = fopen("U:/CprE424/chebychev1.txt","w");
 	for(i = 0; i<numPts; i++){
-		fprintf(fptr,"%Lf	%Lf\n", pts[i],y[i]);
+		fprintf(fptr,"%d %d", pts[i],y[i]);
 	}
-printf("\n3\n");
 	fclose(fptr);
 }
