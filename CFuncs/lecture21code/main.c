06017f5bac85eac4577e5fc1760b065ab05a9a01
Caleb <cbdeboef@iastate.edu>
2022-11-02 21:18:41 -0500

    Finished lecture 21

--- CFuncs/lecture21code/main.c
+++ CFuncs/lecture21code/main.c
@@ -1,9 +1,9 @@
-# include <stdlib .h>
-# include <stdio .h>
-# include <string .h>
-# include <mpi.h>
-# include <math.h>
-# include "vector .h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+#include <math.h>
+#include "matrix.h"
 
 int main(int argc, char* argv []){
 
@@ -12,73 +12,75 @@ int main(int argc, char* argv []){
 	int my_rank;
 	MPI_Init(NULL, NULL);
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank );
-
-	// Grab the global N parameter
-	// and set the local N parameter
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	
+	double a, b, global_T, error;
 	int N;
-	void get_input(int argc , char* argv [], const int my_rank, const int comm_sz, int* N);
-	get_intput(argc, argv, my_rank, comm_sz, &N);
-	const int N_local = N / comm_sz;
+	void get_input(int argc, char* argv[], const int my_rank, const int comm_sz, double* a, double* b, int* N);
+	get_input(argc, argv, my_rank, comm_sz, &a, &b, &N);
 	
 	// Get time
 	double time_start;
-	if(my_rank == 0){ time_start = MPI_Wtime(); }
+	if(my_rank == 0){
+		time_start = MPI_Wtime();
+	}
+	else{ time_start = 0; }	
 
-	// Create partial vector on current processor
-	vector v_local = new_vector(N_local);
-	for(int i=1; i <= N_local; i++){
-		vget(v_local ,i) = sqrt (( double )(i + my_rank * N_local )); 
-	}
-	
-	// Compute 2-norm squared
-	double norm_squared;
-	double norm_squared_local = pow(vget(v_local, 1), 2);
-	for(int i=2; i <= N_local; i++){
-		norm_squared_local += pow(vget(v_local, i), 2); 
-	}
-	
-	// Add local results to the global result on Processor 0
-	if( my_rank != 0){
-		MPI_Send(& norm_squared_local, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD );
+	// Run local trapezoidal rule
+	const double h = (b-a) / ((double)N);
+	const int local_N = N / comm_sz;
+	const double local_a = a + ((double)(my_rank * local_N)) * h;
+	const double local_b = local_a + ((double)local_N) * h;
+	double compTrap(const double a, const double b, const int N, const double h);
+	double local_T = compTrap(local_a, local_b, local_N, h);
+
+	// Add local results to the global result on processor 0
+	if(my_rank != 0){
+		MPI_Send(&local_T, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 	}
 	else{
-		norm_squared = norm_squared_local;
-		for(int i=1; i< comm_sz ; i++){
-			MPI_Recv (&norm_squared_local, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			norm_squared += norm_squared_local;
+		global_T = local_T;
+		for(int i = 1; i < comm_sz; i++){
+			MPI_Recv(&local_T, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			global_T += local_T;
 		}
+		const double Tex = exp(b) - exp(a);
+		error = fabs(Tex - global_T) / fabs(Tex);
 	}
-
-	// Print answer to screen
-	if(my_rank ==0){
+	
+	if(my_rank == 0){
 		double time_end = MPI_Wtime();
 		double time_elapsed = time_end - time_start;
-		printf ("NP = %2i, N = %i, norm_squared = %20.13 e, norm = %20.13 e\n",	comm_sz, N, norm_squared, sqrt(norm_squared));
-		printf ("Elapsed time = %20.13 e\n", time_elapsed);
+		printf("NP = %2i, N = %i, T = %20.13e, |T-Tex| / |Tex| = %20.13e\n", comm_sz, N, global_T, error);
+		printf("Elapsed time = %20.13e\n", time_elapsed);
 	}
-
-	delete_vector(&v_local);
 
 	// End program
 	MPI_Finalize();
 	return 0;
 }
 
-void get_input(int argc, char* argv[], const int my_rank, const int comm_sz, int* N){
+void get_input(int argc, char* argv[], const int my_rank, const int comm_sz, double* a, double* b, int* N){
 	void usage(const char* prog_name);
-	if (my_rank == 0){
-		if(argc != 2){ usage(argv[0]); }
-		*N = strtol(argv[1], NULL, 10);
-		if(*N <= 0) { usage(argv[0]); }
-		if(*N% comm_sz != 0) { usage(argv[0]); }
+	if(my_rank == 0){
+		if(argc != 4){ usage(argv[0]); }
 
-		for (int i=1; i< comm_sz ; i++){
+		*a = strtod(argv[1], NULL);
+		*b = strtod(argv[2], NULL);
+		*N = strtol(argv[3], NULL, 10);
+		if(*N <= 0) {usage(argv[0]); }
+		if(*N%comm_sz != 0){ usage(argv[0]); }
+
+		for(int i = 1; i < comm_sz; i++){
+			MPI_Send(a, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(b, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 			MPI_Send(N, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
 	}
 	else{
-		MPI_Recv (N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(a, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(b, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 }
 
@@ -87,4 +89,18 @@ void usage(const char* prog_name){
 	fprintf(stderr, "N should be positive \n");
 	fprintf(stderr, "N should be exactly divisible by the number of processors \n");
 	exit (1);
+}
+
+double compTrap(const double a, const double b, const int N, const double h){
+	double func(const double x);
+	double T = 0.5 * (func(a) + func(b));
+	for(int i = 1; i < N; i++){
+		T += func(a + ((double)i) * h);
+	}
+
+	return h * T;
+}
+
+double func(const double x){
+	return exp(-pow(10*x,2)) + sin(x);
 }
